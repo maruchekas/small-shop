6ab8afcad80a3066b1081249f74419c3f0c97b6b
maruchekas <salken84@gmail.com>
2023-06-02 16:51:36 +0300

    init

---
+++ src/main/java/com/maruchek/smallshop/SmallShopApplication.java
@@ -0,0 +1,13 @@
+package com.maruchek.smallshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmallShopApplication.class, args);
+    }
+
+}
